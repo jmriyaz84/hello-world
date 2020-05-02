ea20618114868aefed197447700c62d03627d184
Mohammed Riyaz <jmriyaz84@gmail.com>
2020-05-02 09:17:19 +0400

    Printing the numbers

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -1,8 +1,11 @@
 package com;
 
+import java.util.stream.IntStream;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to DevOps");
+		IntStream.range(1,11).forEach(System.out::println);
 	}
 }
